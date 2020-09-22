ee3c924a026cb4f4b9b5932e8b348c15df74f2dc
camilo <jcguzman8548@misena.edu.co>
2020-09-21 19:57:34 -0500

    estilos a la pgina y funciones

---
+++ js/app.js
@@ -0,0 +1,53 @@
+const tijera = document.getElementById("tijera");
+const papel = document.getElementById("papel");
+const piedra = document.getElementById("piedra");
+const resultado = document.getElementById("resultado");
+
+var puntajeUsu = 0;
+var puntajeMaq = 0;
+const azarMaq = document.getElementById("maquina");
+const marcadorUsu = document.getElementById("marcadorUno");
+const marcadorMaq = document.getElementById("marcadorDos");
+
+tijera.addEventListener("click", () => {
+    inicio(tijera.value)
+});
+papel.addEventListener("click", () => {
+    inicio(papel.value)
+});
+piedra.addEventListener("click", () => {
+    inicio(piedra.value)
+});
+
+
+
+function inicio (valor) {
+    let juego = ["piedra","papel","tijera"];
+    let usuario = valor;
+    let maquiba = maquina();
+    azarMaq.textContent = juego[maquiba];
+    puntaje(usuario,maquiba);
+}
+
+function maquina() {
+    let numero = Math.floor(Math.random()*3);
+    if (numero == 3) {
+        return 2
+    }
+
+    return numero;
+}
+
+function puntaje(usuario,maquina) {
+   
+    if (usuario > maquina) {
+        resultado.textContent = "Ganador";
+        puntajeUsu ++
+        marcadorUsu.textContent = puntajeUsu
+
+    }else{
+        resultado.textContent = "Perdio";
+        puntajeMaq ++
+        marcadorMaq.textContent = puntajeMaq
+    }
+}
